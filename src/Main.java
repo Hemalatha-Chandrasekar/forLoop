7d3494bdfcee7602b0bc41404ed57d7462bca421
Hemalatha-Chandrasekar <hema.alliswell@gmail.com>
2024-11-26 21:14:12 -0600

    For loop Commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+    forLoopDemo loopDemo = new forLoopDemo();
+    //loopDemo.loop1();
+    //loopDemo.loop2();
+       // loopDemo.loop3();
+        loopDemo.loop4();
+    }
+
 }
--- src/forLoopDemo.java
+++ src/forLoopDemo.java
@@ -1,4 +1,46 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class forLoopDemo {
+    Scanner in = new Scanner(System.in);
+    public void loop1()
+    {
+        int n = 5;
+        // for loop
+        for (int i = 1; i <= n; ++i) {
+            System.out.println("Java is fun");
+        }
+    }
+    public void loop2(){
+        int sum = 0;
+        int n = 1000;
+        // for loop
+        for (int i = 1; i <= n; ++i) {
+            // body inside for loop
+            sum += i;     // sum = sum + i
+        }
+        System.out.println("Sum = " + sum);
+    }
+    public void loop3(){
+        String original, reverse = ""; // Objects of String class
+        System.out.println("Enter a string/number to check if it is a palindrome");
+        original = in.nextLine();
+        int length = original.length();
+        for ( int i = length - 1; i >= 0; i-- )
+            reverse = reverse + original.charAt(i);
+        if (original.equals(reverse)) {
+            System.out.println("Entered string/number is a palindrome.");
+        }else
+        {
+            System.out.println("Entered string/number isn't a palindrome.");
+        }
+    }
+    public void loop4(){
+        for(int i=1;i<=5;i++) {
+            //System.out.print(i+" ");
+            for (int j = 1; j <= i; j++) {
+                //System.out.print(j+" ");
+                System.out.print("* ");
+            }
+            System.out.println();//new line
+        }
+    }
 }
